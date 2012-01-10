311a82316c2093e250e82e68797ae6e0bbb67de8
yuqq <yuqq.js@gmail.com>
2012-01-11 05:20:00 +0900

    add script-files

---
+++ jquery.detailclick.js
@@ -0,0 +1,150 @@
+/**
+ * jQuery custom event "leftClick, rightClick, wheelClick"
+ * http://jsnt.blog.fc2.com/
+ * 
+ * Copyright (c) 2011 yuqq (yuqq.js@gmail.com)
+ * Dual licensed under the MIT and GPL licenses.
+ *
+ * Date: 2012-01-10
+ */
+(function()
+{
+
+	(function($, LEFT_CLICK, RIGHT_CLICK, WHEEL_CLICK)
+	{
+		
+		var target = null;
+		var modern = ( !$.browser.msie || $.browser.version >= 9 );
+		var state = 0;
+
+		function aim(e)
+		{
+			target = this;
+			return false;
+		}
+		
+		function reset(e)
+		{
+			target = null;
+		}
+		
+		function checkLeft(e)
+		{
+			if ( target == this && ( (modern && e.button == 0) || (!modern && e.button == 1) ) )
+			{
+				$.event.trigger(LEFT_CLICK, e, this);
+			}
+		}
+		
+		function checkRight(e)
+		{
+			if ( target == this && e.button == 2 )
+			{
+				$.event.trigger(RIGHT_CLICK, e, this);
+			}
+		}
+		
+		function checkWheel(e)
+		{
+			if ( target == this && ( (modern && e.button == 1) || (!modern && e.button == 4) ) )
+			{
+				$.event.trigger(WHEEL_CLICK, e, this);
+			}
+		}
+		
+		$.event.special[LEFT_CLICK] = {
+			
+			setup: function()
+			{
+				$.event.add(this, 'mouseup', checkLeft);
+				if ( !state )
+				{
+					$.event.add(this, 'mousedown', aim);
+					$.event.add(document, 'mouseup', reset);
+				}
+				state = state | 1;
+			},
+			
+			teardown: function()
+			{
+				$.event.remove(this, 'mouseup', checkLeft);
+				state = state ^ 1;
+				if ( !state )
+				{
+					$.event.remove(this, 'mousedown', aim);
+					$.event.remove(document, 'mouseup', reset);
+				}
+			}
+			
+		};
+
+		$.event.special[RIGHT_CLICK] = {
+			
+			setup: function()
+			{
+				$.event.add(this, 'mouseup', checkRight);
+				if ( !state )
+				{
+					$.event.add(this, 'mousedown', aim);
+					$.event.add(document, 'mouseup', reset);
+				}
+				state = state | 2;
+			},
+			
+			teardown: function()
+			{
+				$.event.remove(this, 'mouseup', checkRight);
+				state = state ^ 2;
+				if ( !state )
+				{
+					$.event.remove(this, 'mousedown', aim);
+					$.event.remove(document, 'mouseup', reset);
+				}
+			}
+			
+		};
+		
+		$.event.special[WHEEL_CLICK] = {
+			
+			setup: function()
+			{
+				$.event.add(this, 'mouseup', checkWheel);
+				if ( !state )
+				{
+					$.event.add(this, 'mousedown', aim);
+					$.event.add(document, 'mouseup', reset);
+				}
+				state = state | 4;
+			},
+			
+			teardown: function()
+			{
+				$.event.remove(this, 'mouseup', checkWheel);
+				state = state ^ 4;
+				if ( !state )
+				{
+					$.event.remove(this, 'mousedown', aim);
+					$.event.remove(document, 'mouseup', reset);
+				}
+			}
+			
+		};
+	
+		$.fn[LEFT_CLICK] = function(fn)
+		{
+			return fn ? this.bind(LEFT_CLICK, fn) : this.trigger(LEFT_CLICK);
+		};
+	
+		$.fn[RIGHT_CLICK] = function(fn)
+		{
+			return fn ? this.bind(RIGHT_CLICK, fn) : this.trigger(RIGHT_CLICK);
+		};	
+		
+		$.fn[WHEEL_CLICK] = function(fn)
+		{
+			return fn ? this.bind(WHEEL_CLICK, fn) : this.trigger(WHEEL_CLICK);
+		};
+			
+	})(jQuery, 'leftClick', 'rightClick', 'wheelClick');
+
+})();
